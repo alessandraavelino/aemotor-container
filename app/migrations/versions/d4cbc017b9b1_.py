f969d096007ca62748f8ad6cbcd3cbda64b0daa0
Alessandra Avelino <alessavelino123@gmail.com>
2023-04-12 23:40:41 -0300

    fix error

--- app/migrations/versions/d4cbc017b9b1_.py
+++ app/migrations/versions/d4cbc017b9b1_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 7e7c6eafc516
+Revision ID: d4cbc017b9b1
 Revises: 
-Create Date: 2023-04-13 02:03:20.830026
+Create Date: 2023-04-13 02:39:25.438505
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '7e7c6eafc516'
+revision = 'd4cbc017b9b1'
 down_revision = None
 branch_labels = None
 depends_on = None
