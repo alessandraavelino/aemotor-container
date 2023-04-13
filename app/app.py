1a012f37801d15ad693f87345217631f12a8c9eb
Alessandra Avelino <alessavelino123@gmail.com>
2023-04-12 23:08:31 -0300

    Atividade 1 de GCSI

---
+++ app/app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_restful import Api
+from flask_cors import CORS
+
+from resources.endereco import Enderecos
+from resources.funcionario import Funcionarios
+from helpers.database import db, migrate
+
+
+# CORS
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+# SQLAlchemy
+
+# Essa linha abaixo pode provocar conflito de portas e nunca rodar o serviço! achei a pegadinha hehe
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://usr:pwd@localhost:5432/aemotor"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate.init_app(app, db)
+
+api = Api(app)
+
+api.add_resource(Enderecos, '/enderecos')
+api.add_resource(Funcionarios, '/funcionarios')
+
+if __name__ == '__main__':
+    app.run(debug=False)
